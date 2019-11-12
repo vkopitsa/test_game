5733671340c7c766e90ff0d268892bc74acc44d8
Vladimir Kopitsa <v.kopitsa@gmail.com>
2019-11-12 08:11:23 +0200

    save satte

--- front/src/game.ts
+++ front/src/game.ts
@@ -200,45 +200,45 @@ export class Game {
     }
   };
   
-  private drawGrid(
-      ctx: CanvasRenderingContext2D, 
-      minor: number | undefined = undefined, 
-      major: number | undefined = undefined, 
-      stroke: string | undefined = undefined, 
-      fill: string | undefined = undefined,
-  ){
-      minor = minor || 10;
-      major = major || minor * 5;
-      stroke = stroke || "#00FF00";
-      fill = fill || "#009900";
-      ctx.save();
-      ctx.strokeStyle = stroke;
-      ctx.fillStyle = fill;
-      let width = ctx.canvas.width, 
-          height = ctx.canvas.height;
+  // private drawGrid(
+  //     ctx: CanvasRenderingContext2D, 
+  //     minor: number | undefined = undefined, 
+  //     major: number | undefined = undefined, 
+  //     stroke: string | undefined = undefined, 
+  //     fill: string | undefined = undefined,
+  // ){
+  //     minor = minor || 10;
+  //     major = major || minor * 5;
+  //     stroke = stroke || "#00FF00";
+  //     fill = fill || "#009900";
+  //     ctx.save();
+  //     ctx.strokeStyle = stroke;
+  //     ctx.fillStyle = fill;
+  //     let width = ctx.canvas.width, 
+  //         height = ctx.canvas.height;
       
-      for(var x = 0; x < width; x += minor) {
-          ctx.beginPath();
-          ctx.moveTo(x, 0);
-          ctx.lineTo(x, height);
-          ctx.lineWidth = (x % major == 0) ? 0.5 : 0.25;
-          ctx.stroke();
-          if(x % major == 0 ) {
-              ctx.fillText(x.toString(), x, 10);
-          }
-      }
+  //     for(var x = 0; x < width; x += minor) {
+  //         ctx.beginPath();
+  //         ctx.moveTo(x, 0);
+  //         ctx.lineTo(x, height);
+  //         ctx.lineWidth = (x % major == 0) ? 0.5 : 0.25;
+  //         ctx.stroke();
+  //         if(x % major == 0 ) {
+  //             ctx.fillText(x.toString(), x, 10);
+  //         }
+  //     }
 
-      for(var y = 0; y < height; y += minor) {
-          ctx.beginPath();
-          ctx.moveTo(0, y);
+  //     for(var y = 0; y < height; y += minor) {
+  //         ctx.beginPath();
+  //         ctx.moveTo(0, y);
 
-          ctx.lineTo(width, y);
-          ctx.lineWidth = (y % major == 0) ? 0.5 : 0.25;
-          ctx.stroke();
-          if(y % major == 0 ) {
-              ctx.fillText(y.toString(), 0, y + 10);
-          }
-      }
-      ctx.restore();
-  }
+  //         ctx.lineTo(width, y);
+  //         ctx.lineWidth = (y % major == 0) ? 0.5 : 0.25;
+  //         ctx.stroke();
+  //         if(y % major == 0 ) {
+  //             ctx.fillText(y.toString(), 0, y + 10);
+  //         }
+  //     }
+  //     ctx.restore();
+  // }
 }
--- front/src/map.ts
+++ front/src/map.ts
@@ -17,23 +17,7 @@ export class Map {
         ctx.canvas.width = this.width;
         ctx.canvas.height = this.height;
 
-        var rows = ~~(this.width / 44) + 1;
-        var columns = ~~(this.height / 44) + 1;
-
-        var color = "red";
-        ctx.save();
-        ctx.fillStyle = "red";
-        for (var x = 0, i = 0; i < rows; x += 44, i++) {
-            ctx.beginPath();
-            for (var y = 0, j = 0; j < columns; y += 44, j++) {
-                ctx.rect(x, y, 40, 40);
-            }
-            color = (color == "red" ? "blue" : "red");
-            ctx.fillStyle = color;
-            ctx.fill();
-            ctx.closePath();
-        }
-        ctx.restore();
+        this.drawGrid(ctx)
 
         // store the generate map as this image texture
         this.image = new Image();
@@ -43,14 +27,52 @@ export class Map {
         ctx = null;
     }
 
+    private drawGrid(
+        ctx: CanvasRenderingContext2D, 
+        minor: number | undefined = undefined, 
+        major: number | undefined = undefined, 
+        stroke: string | undefined = undefined, 
+        fill: string | undefined = undefined,
+    ){
+        minor = minor || 10;
+        major = major || minor * 5;
+        stroke = stroke || "#00FF00";
+        fill = fill || "#009900";
+        ctx.save();
+        ctx.strokeStyle = stroke;
+        ctx.fillStyle = fill;
+        let width = ctx.canvas.width, 
+            height = ctx.canvas.height;
+        
+        for(var x = 0; x < width; x += minor) {
+            ctx.beginPath();
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, height);
+            ctx.lineWidth = (x % major == 0) ? 0.5 : 0.25;
+            ctx.stroke();
+            // coordinate
+            // if(x % major == 0 ) {
+            //     ctx.fillText(x.toString(), x, 10);
+            // }
+        }
+  
+        for(var y = 0; y < height; y += minor) {
+            ctx.beginPath();
+            ctx.moveTo(0, y);
+  
+            ctx.lineTo(width, y);
+            ctx.lineWidth = (y % major == 0) ? 0.5 : 0.25;
+            ctx.stroke();
+            // coordinate
+            // if(y % major == 0 ) {
+            //     ctx.fillText(y.toString(), 0, y + 10);
+            // }
+        }
+        ctx.restore();
+    }
+
     // draw the map adjusted to camera
     public draw(context: CanvasRenderingContext2D, xView: number, yView: number) {
-        // easiest way: draw the entire map changing only the destination coordinate in canvas
-        // canvas will cull the image by itself (no performance gaps -> in hardware accelerated environments, at least)
-        /*context.drawImage(this.image, 0, 0, this.image.width, this.image.height, -xView, -yView, this.image.width, this.image.height);*/
-
-        // didactic way ( "s" is for "source" and "d" is for "destination" in the variable names):
-
         if (!this.image) {
             return;
         }
@@ -82,5 +104,29 @@ export class Map {
         dHeight = sHeight;
 
         context.drawImage(this.image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+
+        // add current coordinate 
+        const minor = 10;
+        const major = minor * 5;
+        const stroke = "#00FF00";
+        const fill = "#009900";
+        context.save();
+        context.strokeStyle = stroke;
+        context.fillStyle = fill;
+        let width = context.canvas.width, 
+            height = context.canvas.height;
+        
+        for(var x = 0; x < width; x += minor) {
+            if(x % major == 0 ) {
+                context.fillText((~~(xView + x)).toString(), x, 10);
+            }
+        }
+  
+        for(var y = 0; y < height; y += minor) {
+            if(y % major == 0 ) {
+                context.fillText((~~(yView + y)).toString(), 0, y + 10);
+            }
+        }
+        context.restore();
     }
 }
